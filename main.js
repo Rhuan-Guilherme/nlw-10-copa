961f84828e4c181a1c55f32e19587757522107e1
Rhuan Guilherme <rhuan23032004@gmail.com>
2022-11-06 12:26:32 -0300

    arrumando bandeiras e horarios

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ function createCard(date, day, games){
 
 document.querySelector('#cards').innerHTML = 
 
-        createCard('24/11', 'quinta', createGame('brasil', '16:00', 'serbia') + createGame('suica', '16:00', 'camaroes')) + 
-        createCard('28/11', 'segunda', createGame('brasil', '13:00', 'suica')) + 
-        createCard('02/12', 'sexta', createGame('brasil', '16:00', 'camaroes'))
+        createCard('24/11', 'quinta', createGame('brasil', '16:00', 'serbia') + createGame('suica', '07:00', 'camaroes') + createGame('portugal', '13:00', 'gana') + createGame('uruguai', '10:00', 'coreia')) + 
+        createCard('28/11', 'segunda', createGame('brasil', '13:00', 'suica') + createGame('camaroes', '07:00', 'serbia') + createGame('coreia', '10:00', 'gana') + createGame('portugal', '16:00', 'uruguai')) + 
+        createCard('02/12', 'Sexta', createGame('brasil', '16:00', 'camaroes') + createGame('coreia', '12:00', 'portugal') + createGame('serbia', '16:00', 'suica') + createGame('gana', '12:00', 'uruguai')) 
+        
     
